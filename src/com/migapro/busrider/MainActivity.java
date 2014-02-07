472fc88f631f65b858c064960ef50cbbd20c7da1
Miguel Kano <miga88k@gmail.com>
2014-02-06 22:43:30 -0600

    set ActionBar tabs

--- src/com/migapro/busrider/MainActivity.java
+++ src/com/migapro/busrider/MainActivity.java
@@ -1,15 +1,24 @@
 package com.migapro.busrider;
 
 import android.os.Bundle;
-import android.app.Activity;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBar.Tab;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ActionBarActivity implements ActionBar.TabListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		final ActionBar actionBar = getSupportActionBar();
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+		actionBar.addTab(actionBar.newTab().setText("Schedule").setTabListener(this));
+		actionBar.addTab(actionBar.newTab().setText("Route").setTabListener(this));
+		actionBar.addTab(actionBar.newTab().setText("Elapsed\nTime").setTabListener(this));
 	}
 
 	@Override
@@ -17,6 +26,24 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	@Override
+	public void onTabReselected(Tab arg0, FragmentTransaction arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTabSelected(Tab arg0, FragmentTransaction arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTabUnselected(Tab arg0, FragmentTransaction arg1) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
