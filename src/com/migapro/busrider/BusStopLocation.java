7bd55d5ee42d551c73db3607b5b05e4fa6248539
miga <miguel.g.kano@gmail.com>
2014-06-16 20:45:31 -0500

    add BusStopLocation class

---
+++ src/com/migapro/busrider/BusStopLocation.java
@@ -0,0 +1,20 @@
+package com.migapro.busrider;
+
+import com.google.android.gms.maps.model.LatLng;
+
+/**
+ * BusStopLocation class represents bus stop location in Google Maps.
+ * @author Miga
+ *
+ */
+public class BusStopLocation {
+	
+	private String mTitle;
+	private LatLng mLatLng;
+	
+	public BusStopLocation(String title, int latitude, int longtitude) {
+		mTitle = title;
+		mLatLng = new LatLng(latitude, longtitude);
+	}
+
+}
