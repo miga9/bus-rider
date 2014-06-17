f2b4ec086ceaa4caa88daad57a2888796804da84
miga <miguel.g.kano@gmail.com>
2014-06-16 20:45:05 -0500

    add bus class

---
+++ src/com/migapro/busrider/Bus.java
@@ -0,0 +1,20 @@
+package com.migapro.busrider;
+
+import java.util.ArrayList;
+
+/**
+ * Bus class represents a route its buses take.
+ * @author Miga
+ *
+ */
+public class Bus {
+	
+	private String mBusName;
+	private ArrayList<DepartingPoint> mDepartingPoints;
+	
+	public Bus(String busName, ArrayList<DepartingPoint> departingPoints) {
+		mBusName = busName;
+		mDepartingPoints = departingPoints;
+	}
+
+}
