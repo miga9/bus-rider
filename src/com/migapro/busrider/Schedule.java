c202cc0b1c697493506384d80f9a29454bb21791
miga <miguel.g.kano@gmail.com>
2014-06-16 20:46:08 -0500

    add Schedule class

---
+++ src/com/migapro/busrider/Schedule.java
@@ -0,0 +1,22 @@
+package com.migapro.busrider;
+
+import java.util.ArrayList;
+
+import android.text.format.Time;
+
+/**
+ * Schedule class represents a time table for bus stop.
+ * @author Miga
+ *
+ */
+public class Schedule {
+	
+	private String mDaysOfOperation;
+	private ArrayList<Time> mTimes;
+	
+	public Schedule(String daysOfOperation, ArrayList<Time> times) {
+		mDaysOfOperation = daysOfOperation;
+		mTimes = times;
+	}
+
+}
