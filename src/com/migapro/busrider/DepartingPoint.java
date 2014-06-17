9fbdeca41e3235c0a211785e0af0dad25423beb3
miga <miguel.g.kano@gmail.com>
2014-06-16 20:45:52 -0500

    add DepartingPoint class

---
+++ src/com/migapro/busrider/DepartingPoint.java
@@ -0,0 +1,23 @@
+package com.migapro.busrider;
+
+import java.util.ArrayList;
+
+/**
+ * DepartingPoint represents a bus stop where it contains departing times.
+ * @author Miga
+ *
+ */
+public class DepartingPoint {
+	
+	private String mDepartFrom;
+	private ArrayList<Schedule> mSchedules;
+	private ArrayList<BusStopLocation> mBusStops;
+	
+	public DepartingPoint(String departFrom, ArrayList<Schedule> schedules,
+			ArrayList<BusStopLocation> busStops) {
+		mDepartFrom = departFrom;
+		mSchedules = schedules;
+		mBusStops = busStops;
+	}
+
+}
