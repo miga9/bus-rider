f77f825345dae7a4b4837e8b79d68c45b326c714
miga <miguel.g.kano@gmail.com>
2015-01-16 21:43:41 -0600

    Use appcompat Toolbar as actionbar

--- bus_rider_app/app/src/main/java/com/migapro/busrider/ui/MainActivity.java
+++ bus_rider_app/app/src/main/java/com/migapro/busrider/ui/MainActivity.java
@@ -10,15 +10,20 @@ import android.app.ActionBar.OnNavigationListener;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.astuetz.PagerSlidingTabStrip;
 import com.migapro.busrider.R;
 import com.migapro.busrider.models.Bus;
 import com.migapro.busrider.parser.BusXmlPullParser;
 
-public class MainActivity extends Activity implements OnNavigationListener {
+public class MainActivity extends ActionBarActivity implements AdapterView.OnItemSelectedListener {
 
 	private ViewPager mViewPager;
 	private BusXmlPullParser mParser;
@@ -29,10 +34,10 @@ public class MainActivity extends Activity implements OnNavigationListener {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		final ActionBar actionBar = getActionBar();
-		actionBar.setDisplayShowTitleEnabled(false);
-		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
 
 		try {
 			mParser = new BusXmlPullParser();
@@ -45,7 +50,9 @@ public class MainActivity extends Activity implements OnNavigationListener {
 		
 		ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<String>(this, R.layout.spinner_title, mBusNames);
 		spinnerAdapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
-		actionBar.setListNavigationCallbacks(spinnerAdapter, this);
+        Spinner titleSpinner = (Spinner) findViewById(R.id.title_spinner);
+        titleSpinner.setAdapter(spinnerAdapter);
+        titleSpinner.setOnItemSelectedListener(this);
 		
 		mViewPager = (ViewPager) findViewById(R.id.view_pager);
 		mViewPager.setAdapter(new ViewPagerAdapter(getFragmentManager()));
@@ -60,17 +67,19 @@ public class MainActivity extends Activity implements OnNavigationListener {
 		return true;
 	}
 
-	@Override
-	public boolean onNavigationItemSelected(int itemPosition, long timeId) {
-		try {
-			mCurrentBus = mParser.readABusData(getAssets().open("data/bus_data.xml"), 0);
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (XmlPullParserException e) {
-			e.printStackTrace();
-		}
-		
-		return false;
-	}
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        try {
+            mCurrentBus = mParser.readABusData(getAssets().open("data/bus_data.xml"), 0);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (XmlPullParserException e) {
+            e.printStackTrace();
+        }
+    }
 
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
 }
