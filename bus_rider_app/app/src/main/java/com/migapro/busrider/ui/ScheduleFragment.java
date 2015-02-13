75a8d37a6d29433d980c0df892ed27c0478cf6f2
miga <miguel.g.kano@gmail.com>
2015-02-12 19:41:57 -0600

    Display default text when time-table is unavailable

--- bus_rider_app/app/src/main/java/com/migapro/busrider/ui/ScheduleFragment.java
+++ bus_rider_app/app/src/main/java/com/migapro/busrider/ui/ScheduleFragment.java
@@ -6,15 +6,24 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.migapro.busrider.R;
 import com.migapro.busrider.utility.Constants;
 
 public class ScheduleFragment extends ListFragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+
         int scheduleIndex = getArguments().getInt(Constants.FRAGMENT_SCHEDULE_INDEX_KEY);
         setListAdapter(new ScheduleAdapter(getActivity(), ((MainActivity) getActivity()).getTimeList(scheduleIndex)));
 
         return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        setEmptyText(getActivity().getResources().getString(R.string.schedule_empty_text));
     }
 }
