4935ff8cdc5adb4e75fc7a674fb732467ac23189
miga <miguel.g.kano@gmail.com>
2015-04-01 20:53:04 -0500

    Load blank google maps

--- bus_rider_app/app/src/main/java/com/migapro/busrider/ui/MapActivity.java
+++ bus_rider_app/app/src/main/java/com/migapro/busrider/ui/MapActivity.java
@@ -6,25 +6,34 @@ import android.support.v7.widget.Toolbar;
 
 import com.google.android.gms.analytics.HitBuilders;
 import com.google.android.gms.analytics.Tracker;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.migapro.busrider.BusRiderApplication;
 import com.migapro.busrider.R;
 import com.migapro.busrider.utility.Constants;
 
-public class MapActivity extends ActionBarActivity {
+public class MapActivity extends ActionBarActivity implements OnMapReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
+        initViews();
+    }
+
+    private void initViews() {
         String title = getIntent().getStringExtra(Constants.MAP_TITLE_KEY);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle(title);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-    }
 
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
 
     @Override
     protected void onStart() {
@@ -33,5 +42,10 @@ public class MapActivity extends ActionBarActivity {
         Tracker tracker = ((BusRiderApplication) getApplication()).getTracker();
         tracker.setScreenName(Constants.ANALYTICS_MAP_SCREEN);
         tracker.send(new HitBuilders.ScreenViewBuilder().build());
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        
     }
 }
