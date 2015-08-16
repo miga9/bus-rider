83bdc9cf150b3e8f138c2065d5fb37f8056b99d9
miga <miguel.g.kano@gmail.com>
2015-08-16 10:30:37 -0500

    Display network call failed dialog

--- bus_rider_app/app/src/main/java/com/migapro/busrider/ui/MainActivity.java
+++ bus_rider_app/app/src/main/java/com/migapro/busrider/ui/MainActivity.java
@@ -329,6 +329,10 @@ public class MainActivity extends ActionBarActivity implements DataAsyncTask.OnD
             updateCurrentBusUI();
 
             Util.saveLastUpdatedDate(this);
+        } else {
+            MsgDialog failureDialog = MsgDialog.newInstance(DIALOG_MSG_ID_FAILURE, R.string.download_failure_title, R.string.download_failure_msg, R.string.download_failure_pos);
+            failureDialog.setCancelable(false);
+            failureDialog.show(getFragmentManager(), "failureDialog");
         }
     }
 
